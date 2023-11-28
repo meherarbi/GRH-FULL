79957328e049036549e2807fe073ac91f5bc3a1e
Meher Arby <meher.arbi@gmail.com>
2023-11-28 10:18:49 +0100

    add timesheet

--- src/Controller/TimeSheetController.php
+++ src/Controller/TimeSheetController.php
@@ -16,9 +16,10 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 #[Route('/api/timesheet')]
 class TimeSheetController extends AbstractController
 {
-    #[Route('/', name: 'create_timesheet', methods: ['POST'])]
+    #[Route('', name: 'create_timesheet', methods: ['POST'])]
     public function create(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse {
         $timeSheet = $serializer->deserialize($request->getContent(), TimeSheet::class, 'json');
+        $timeSheet->setUser($this->getUser());
 
         $errors = $validator->validate($timeSheet);
         if (count($errors) > 0) {
@@ -31,13 +32,13 @@ class TimeSheetController extends AbstractController
         return $this->json($timeSheet, Response::HTTP_CREATED);
     }
 
-    #[Route('/', name: 'list_timesheets', methods: ['GET'])]
+    #[Route('/list', name: 'list_timesheets', methods: ['GET'])]
     public function list(TimeSheetRepository $timeSheetRepository): JsonResponse {
         $timeSheets = $timeSheetRepository->findAll();
         return $this->json($timeSheets);
     }
 
-    #[Route('/{id}', name: 'update_timesheet', methods: ['PUT'])]
+    #[Route('/edit/{id}', name: 'update_timesheet', methods: ['PUT'])]
     public function update(Request $request, TimeSheet $timeSheet, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse {
         $updatedTimeSheet = $serializer->deserialize($request->getContent(), TimeSheet::class, 'json', ['object_to_populate' => $timeSheet]);
 
@@ -51,7 +52,7 @@ class TimeSheetController extends AbstractController
         return $this->json($updatedTimeSheet);
     }
 
-    #[Route('/{id}', name: 'delete_timesheet', methods: ['DELETE'])]
+    #[Route('/delete/{id}', name: 'delete_timesheet', methods: ['DELETE'])]
     public function delete(TimeSheet $timeSheet, EntityManagerInterface $entityManager): JsonResponse {
         $entityManager->remove($timeSheet);
         $entityManager->flush();
